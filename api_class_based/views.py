ed97bf21c7d11ed13a63995ae0b961f45885a61d
Abin Jumran <abin@yougotagift.com>
2024-01-30 09:51:28 +0530

    modified class based api

--- api_class_based/views.py
+++ api_class_based/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import get_object_or_404, ListCreateAPIView, \
+from rest_framework.generics import ListCreateAPIView, \
     RetrieveUpdateDestroyAPIView
 from post.models import Post
 from .serializers import PostSerializer
